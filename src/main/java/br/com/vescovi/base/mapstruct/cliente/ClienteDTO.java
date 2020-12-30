d8e7603fa5d441f8e3f59db0cecc06cfa1352f77
paulohvescovi <paulo20091994@gmail.com>
2020-12-29 21:23:06 -0300

    @Valid and DTO Validation

--- src/main/java/br/com/vescovi/base/mapstruct/cliente/ClienteDTO.java
+++ src/main/java/br/com/vescovi/base/mapstruct/cliente/ClienteDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -13,7 +16,12 @@ import java.io.Serializable;
 public class ClienteDTO implements Serializable {
 
     private Long id;
+
+    @NotEmpty(message = "nome nao pode ser vazio")
     private String nome;
+
+    @CNPJ
+    @NotEmpty(message = "cnpj deve ser informado")
     private String cnpj;
 
 }
