5dc23e19faa5547f25e08a789364ccc33cd3acda
paulohvescovi <paulo20091994@gmail.com>
2021-01-01 11:57:29 -0300

    mais alguns exemplos de teste unitario JPA

--- src/main/java/br/com/vescovi/base/mapstruct/cliente/ClienteRepository.java
+++ src/main/java/br/com/vescovi/base/mapstruct/cliente/ClienteRepository.java
@@ -2,5 +2,10 @@ package br.com.vescovi.base.mapstruct.cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findFirstByNomeContaining(String nome);
+
 }
--- src/test/java/br/com/vescovi/base/mapstruct/cliente/ClienteRepositoryTest.java
+++ src/test/java/br/com/vescovi/base/mapstruct/cliente/ClienteRepositoryTest.java
@@ -1,15 +1,20 @@
 package br.com.vescovi.base.mapstruct.cliente;
 
+import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("Testes para cliente Repository")
+@Log4j2
 class ClienteRepositoryTest {
 
     @Autowired private ClienteRepository clienteRepository;
@@ -24,6 +29,59 @@ class ClienteRepositoryTest {
         Assertions.assertThat(clienteSaved.getId()).isNotNull();
         Assertions.assertThat(clienteSaved.getNome()).isEqualTo(clienteForSave.getNome());
 
+    }
+
+    @Test
+    @DisplayName("Testando atualizar cliente no banco de dados")
+    void save_UpdateCliente_WhenSuccessful(){
+        Cliente clienteForSave = createCliente();
+        Cliente clienteSaved = clienteRepository.save(clienteForSave);
+
+        clienteSaved.setNome("Cliente Alterado");
+        Cliente clienteAtualizado = clienteRepository.save(clienteSaved);
+
+        Assertions.assertThat(clienteAtualizado).isNotNull();
+        Assertions.assertThat(clienteAtualizado.getId()).isNotNull();
+        Assertions.assertThat(clienteAtualizado.getNome()).isEqualTo(clienteSaved.getNome());
+
+    }
+
+    @Test
+    @DisplayName("Testando deletar cliente no banco de dados")
+    void delete_UpdateCliente_WhenSuccessful(){
+        Cliente clienteForSave = createCliente();
+        Cliente clienteSaved = clienteRepository.save(clienteForSave);
+
+        clienteRepository.delete(clienteSaved);
+
+        Optional<Cliente> clienteOptional = this.clienteRepository.findById(clienteSaved.getId());
+
+        Assertions.assertThat(clienteOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Buscando pelo  nome no banco de dados")
+    void findByNome_ReturnListOfCliente_WhenSuccessful(){
+        Cliente clienteforSave1 = createCliente();
+        Cliente clienteSaved1 = clienteRepository.save(clienteforSave1);
+
+        List<Cliente> clienteList = clienteRepository.findFirstByNomeContaining("Cliente");
+
+        Assertions.assertThat(clienteList).isNotEmpty();
+        Assertions.assertThat(clienteList).contains(clienteforSave1);
+        Assertions.assertThat(clienteList.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Buscando pelo nome que nao existe no banco de dados")
+    void findByNome_ReturnEmptyListOfCliente_WhenSuccessful(){
+        Cliente clienteforSave1 = createCliente();
+        Cliente clienteSaved1 = clienteRepository.save(clienteforSave1);
+
+        List<Cliente> clienteList = clienteRepository.findFirstByNomeContaining("Paulo");
+
+        Assertions.assertThat(clienteList).isNotNull();
+        Assertions.assertThat(clienteList).isEmpty();
     }
 
     private Cliente createCliente(){
